ec87009b1a34cf30105009dfedb6147219dbfb6d
Eka Bachrudin <eka.nursa@salt.co.id>
2024-11-02 13:51:45 +0700

    Setup dont & color tailwind

--- src/pages/Doc/Doc.tsx
+++ src/pages/Doc/Doc.tsx
@@ -6,6 +6,15 @@ const Doc: React.FC = () => {
     <div className={styles.docContainer}>
       <h1 className={styles.title}>Documentation Page</h1>
       <p className={styles.description}>This is the documentation page.</p>
+
+      <div className="p-4">
+        <h1 className="font-poppins-thin text-2xl">Documentation Page (Bold)</h1>
+        <p className="poppins-regular">This is regular text.</p>
+        <p className="poppins-medium">This is medium-weight text.</p>
+        <p className="poppins-light">This is light-weight text.</p>
+        <p className="poppins-extrabold">This is extra-bold text.</p>
+      </div>
+
     </div>
   );
 };
